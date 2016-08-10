ab9ff142f8a0edaf18dad4ae891fc6202f042b9d
Travis Lee <stunnel.info@gmail.com>
2016-08-10 16:19:57 +0800

    fix bugs

--- modules/COMMANDI_TEST.py
+++ modules/COMMANDI_TEST.py
@@ -5,16 +5,14 @@ import variable
 import time
 
 def check_commandi_usecase(info,domain,method,i):
-    print "----------------"
+    print("-" * 16)
 #    print i,":",info
     res = check_usecase("/search.php",method,{"keyword":info},None,domain)
-    
+
     if res == 403:
-        print i,":",res,"   usecase:",info
+        print("%s : %s    usecase: %s" % (i, res, info))
     else:
-        print '\033[1;31;40m'
-        print i,":",res,"   usecase:",info
-        print '\033[0m'
+        print("%s %s : %s    usecase: %s %s" % ("\033[1;31;40m", i, res, info, "\033[0m"))
 
 def COMMANDI_TEST(domain,method,usecase):
     commandi_file = open(usecase)
--- modules/DOWNLOAD_TEST.py
+++ modules/DOWNLOAD_TEST.py
@@ -4,8 +4,8 @@ from lib.request_util import *
 import variable
 import time
 
-def check_download_usecase(info,domain,i):
-    print "----------------"
+def check_download_usecase(info,domain,method,i):
+    print("-" * 16)
 #    print i,":",info
     info = "/"+info
     res = check_usecase(info,method,None,None,domain)
@@ -13,9 +13,7 @@ def check_download_usecase(info,domain,i):
     if res == 403:
         print i,":",res,"   usecase:",info
     else:
-        print '\033[1;31;40m'
-        print i,":",res,"   usecase:",info
-        print '\033[0m'
+        print '\033[1;31;40m', i,":",res,"   usecase:",info, '\033[0m'
 
 def DOWNLOAD_TEST(domain,method,usecase):
     download_file = open(usecase)
